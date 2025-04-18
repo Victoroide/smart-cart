3e8fa910df848466e8bbfa84faa6be9385be2722
Victoroide <cvictorhugo39@gmail.com>
2025-04-17 21:56:07 -0400

    feat: Remove unused Swagger security definitions to streamline API documentation

--- base/settings.py
+++ base/settings.py
@@ -181,18 +181,6 @@ REST_FRAMEWORK = {
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH': False,
     'VALIDATOR_URL': None,
-    'SECURITY_DEFINITIONS': {
-        'Bearer': {
-            'type': 'apiKey',
-            'name': 'Authorization',
-            'in': 'header'
-        }
-    },
-    'SECURITY_REQUIREMENTS': [
-        {'Bearer': []}
-    ],
-    'USE_SESSION_AUTH': False,
-    'JSON_EDITOR': True,
     'PERSIST_AUTH': True,
 }
 PINECONE_INDEX_NAME = config('PINECONE_INDEX_NAME')

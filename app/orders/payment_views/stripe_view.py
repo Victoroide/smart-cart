17e60ed2895d48c121151df3951d4ab18e8004f5
Victoroide <cvictorhugo39@gmail.com>
2025-04-19 17:38:13 -0400

    feat: Remove success and cancel URLs from Stripe checkout session creation

--- app/orders/payment_views/stripe_view.py
+++ app/orders/payment_views/stripe_view.py
@@ -32,10 +32,6 @@ class StripeCheckoutView(APIView):
                         'quantity': 1,
                     }],
                     mode='payment',
-                    success_url=(
-                        f'{settings.FRONTEND_URL}/customer/carrito?payment=success'
-                    ),
-                    cancel_url=f'{settings.FRONTEND_URL}/customer/carrito?payment=cancel',
                     metadata={
                         'order_id': order.id
                     }

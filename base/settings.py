66408a0201379755bf3e4381b4de98668324352e
Victoroide <cvictorhugo39@gmail.com>
2025-04-20 17:45:30 -0400

    feat: Add JWT settings for access and refresh token lifetimes

--- base/settings.py
+++ base/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from urllib.parse import urlparse
 from decouple import config
+from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -176,6 +177,11 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.CustomPagination',
     'PAGE_SIZE': 20,
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(weeks=2),
 }
 
 SWAGGER_SETTINGS = {

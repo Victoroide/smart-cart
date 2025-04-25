613bc067d60c82d69c6c34c1b2cc06b170bb8402
Victoroide <cvictorhugo39@gmail.com>
2025-04-25 18:40:13 -0400

    refactor: Remove default ACL setting for S3 storage to enhance security

--- base/storage.py
+++ base/storage.py
@@ -14,7 +14,6 @@ class StaticStorage(S3Boto3Storage):
 
 class PublicMediaStorage(S3Boto3Storage):
     location = 'public/media'
-    default_acl = 'public-read'
     file_overwrite = False
     querystring_auth = False
     

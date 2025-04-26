362a6a8edce456d00ded9ebb624d00db7cb62866
Victoroide <cvictorhugo39@gmail.com>
2025-04-26 19:31:22 -0400

    feat: Enhance LoggerService model with IP address field and update serializer and viewset for improved logging functionality

--- core/models/logger_service_model.py
+++ core/models/logger_service_model.py
@@ -8,8 +8,14 @@ class LoggerService(TimestampedModel):
     table_name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     level = models.CharField(max_length=20, default='INFO')
+    ip_address = models.GenericIPAddressField(null=True, blank=True, verbose_name='IP Address')
 
     def save(self, *args, **kwargs):
         if self.user is None:
             self.user_id = 1
         super().save(*args, **kwargs)
+        
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Logger Service'
+        verbose_name_plural = 'Logger Services'
